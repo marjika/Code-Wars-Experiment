88348802f2392a3ed0533185045a852569edc51b
marjika <bluegoo5@hotmail.com>
2019-04-06 15:21:48 -0600

    ASCII tree done, mostly, I think

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Deploying Code Wars Activities
+# Deploying Codewars Activities
 
-Taking some code wars activities and expanding
+Taking some Codewars activities and expanding them to practice and continue learning the many tools to use in designing and creating web pages.  
 
 ### Using code wars ideas to create webpages
+
+Codewars (https://www.codewars.com) is a site designed to help programmers practice their skills and learn new ones through fun and interesting activities. This repository is an ongoing project in which I'm attempting to realize the potential fun of some of the activities that I completed.  It helps me to practice javascript, css, html, canvas, classes, user experience, and other new technologies that I incorporate into the webpages.
 
 ##### About this Application
 
--- js/tree.js
+++ js/tree.js
@@ -1,59 +1,60 @@
 $(document).ready(function(){
 
-        function customChristmasTree(chars,n){
-            var answer = "";
-            var charsIndex = 0;
-            for (var i=0; i<n; i++) {
-                answer = answer + " ".repeat(n-i-1);
-                var charsStr = "";
-                while (charsStr.length < ((i*2)+1)) {
-                    charsStr = charsStr + chars[charsIndex] + " ";
-                    charsIndex++;
-                    if (charsIndex>=chars.length){
-                        charsIndex = 0;
-                    }
+    //function which draws the tree and displays it
+    function customChristmasTree(chars,n){
+        var answer = "";
+        var charsIndex = 0;
+        for (var i=0; i<n; i++) {
+            answer = answer + " ".repeat(n-i-1);
+            var charsStr = "";
+            while (charsStr.length < ((i*2)+1)) {
+                charsStr = charsStr + chars[charsIndex] + " ";
+                charsIndex++;
+                if (charsIndex>=chars.length){
+                    charsIndex = 0;
                 }
-                answer = answer + charsStr.slice(0, -1) + '\n';
             }
-            answer = answer + (" ".repeat(n-1) + '|' + '\n').repeat(Math.floor(n/4));
-            console.log(answer.slice(0,-1));
-            $("#result").append(answer.slice(0,-1));
-            //return answer.slice(0,-1);
+            answer = answer + charsStr.slice(0, -1) + '\n';
         }
+        answer = answer + (" ".repeat(n-1) + '|' + '\n').repeat(Math.floor(n/4));
+        console.log(answer.slice(0,-1));
+        $("#result").append(answer.slice(0,-1));
+    }
 
-        $('#tree-input').click(function(){
-            $("#result").empty();
-            console.log($("#rows-input").val());
-            if (Number.isInteger(Math.abs(Math.floor(parseInt($("#rows-input").val().trim()))))) {
-                var rows = Math.abs(Math.floor(parseInt($("#rows-input").val().trim())));
-                if (rows<34) {
-                    var decor = $("#symbols-input").val().trim();
-                    console.log(rows,decor);
-                    customChristmasTree(decor, rows);
-                }
-                else {
-                    displayModal("Please enter an integer from 3 to 33 for the number of rows.")
-                } 
+    //Error handling and button click functioning
+    $('#tree-input').click(function(){
+        $("#result").empty();
+        console.log($("#rows-input").val());
+        if (Number.isInteger(Math.abs(Math.floor(parseInt($("#rows-input").val().trim()))))) {
+            var rows = Math.abs(Math.floor(parseInt($("#rows-input").val().trim())));
+            var decor = $("#symbols-input").val().trim();
+            if (rows<34 && decor!=="") {
+                customChristmasTree(decor, rows);
             }
             else {
-                displayModal("Please enter an integer for the number of rows.")
-            }        
-            $("#rows-input").val('');
-            $("#symbols-input").val('');
-        });
-
-        function displayModal(text) {
-            $("#modal-caption").html("<p>" + text + "</p>");
-            $("#restart-button").show();
-            $("#myModal").show();
+                displayModal("Please enter an integer from 3 to 33 for the number of rows and some characters for decoration.")
+            } 
         }
+        else {
+            displayModal("Please enter an integer for the number of rows.")
+        }        
+        $("#rows-input").val('');
+        $("#symbols-input").val('');
+    });
 
-        // Get the element that closes the modal
-        var closeButton = document.getElementById("close");
+    //error-handling modal function
+    function displayModal(text) {
+        $("#modal-caption").html("<h3>" + text + "</h3>");
+        $("#restart-button").show();
+        $("#myModal").show();
+    }
 
-        // When the user clicks on closeButton, close the modal
-        closeButton.onclick = function() {
-            $("#myModal").hide();
-        }
+    // Get the element that closes the modal
+    var closeButton = document.getElementById("close");
+
+    // When the user clicks on closeButton, close the modal
+    closeButton.onclick = function() {
+        $("#myModal").hide();
+    }
 
 });
